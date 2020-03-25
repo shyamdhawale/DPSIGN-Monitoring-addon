f116b4794bb12adfeeb2ef40fda388bba2883e2d
shyamdhawale <32157028+shyamdhawale@users.noreply.github.com>
2020-03-25 15:58:23 +0530

    Update screenshot.sh

--- roles/screenshot/files/screenshot.sh
+++ roles/screenshot/files/screenshot.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
-#                             _
-#    ____                    | |
-#   / __ \__      _____  _ __| | __ ____
-#  / / _` \ \ /\ / / _ \| '__| |/ /|_  /
-# | | (_| |\ V  V / (_) | |  |   <  / /
-#  \ \__,_| \_/\_/ \___/|_|  |_|\_\/___|
-#   \____/
-#                  http://www.atworkz.de
-#                        info@atworkz.de
-#
-# Screenly OSE Monitoring Add-on
+
+#  Add-on
 
 _VERSION="2"
 _WAIT_TIME="40"
@@ -18,7 +9,7 @@ while [[ $# -gt 0 ]] ; do
   case $1 in
     -h|-\?|--help)
       echo
-			echo "Screenshot Add-on for Screenly OSE Monitoring."
+			echo "Screenshot Add-on for DPSIGN."
       echo
 			echo "With this add-on it is possible to create a screenshot of the current "
       echo "HDMI output and make it available at the address 0.0.0.0:9020."
@@ -40,13 +31,13 @@ while [[ $# -gt 0 ]] ; do
   esac
 done
 
-echo "Screenshot Add-on for Screenly OSE Monitoring"
+echo "Screenshot Add-on for DPSIGN"
 cp /var/www/html/addon/booting.png /var/www/html/addon/screen/screenshot.png
 echo "wait $_WAIT_TIME sec..."
 sleep "$_WAIT_TIME";
 echo "start screenshots..."
 while true; do
-   raspi2png --pngname /var/www/html/addon/screen/screenshot_tmp.png  || cp -f /var/www/html/addon/error.png /var/www/html/addon/screen/screenshot_tmp.png
+   raspi2png -h 200 -w 400 -c 5 --pngname /var/www/html/addon/screen/screenshot_tmp.png  || cp -f /var/www/html/addon/error.png /var/www/html/addon/screen/screenshot_tmp.png
    cp -f /var/www/html/addon/screen/screenshot_tmp.png /var/www/html/addon/screen/screenshot.png
    sleep 3;
 done
